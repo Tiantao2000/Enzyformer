4d87edb3b6449710ae3cffb711b29b4ff8bd29f6
Tiantao2000 <75165033+Tiantao2000@users.noreply.github.com>
2025-09-19 15:50:48 +0800

    Update finetune-backward.sh

--- Retrosynthesis/finetune-backward.sh
+++ Retrosynthesis/finetune-backward.sh
@@ -19,14 +19,14 @@ python -m molbart.fine_tune \
 
 ckpt_path=tb_logs/backward_prediction/version_0/checkpoints
 ckpt_from=${ckpt_path}/last.ckpt
-ckpt_to=${DIR}/model/finetune_USPTO_FULL.ckpt
+ckpt_to=${DIR}/ckpt/finetune_USPTO_FULL.ckpt
 echo cp $ckpt_from to $ckpt_to
 cp "${ckpt_from}" "${ckpt_to}"
 
 python -m molbart.fine_tune \
   datamodule=[molbart.data.seq2seq_data.Uspto50DataModule] \
   data_path=${DIR}/data/bioformer_wo.pkl \
-  model_path=${DIR}/chemformer_models/finetune/finetune_USPTO_FULL.ckpt \
+  model_path=${DIR}/ckpt/finetune_USPTO_FULL.ckpt \
   task=backward_prediction \
   n_epochs=60 \
   learning_rate=0.001 \
@@ -38,7 +38,7 @@ python -m molbart.fine_tune \
 
 ckpt_path=tb_logs/backward_prediction/version_1/checkpoints
 ckpt_from=${ckpt_path}/last.ckpt
-ckpt_to=${DIR}/chemformer_models/finetune/last_USPTO_FULL.ckpt
+ckpt_to=${DIR}/ckpt/last_USPTO_FULL.ckpt
 echo cp $ckpt_from to $ckpt_to
 cp "${ckpt_from}" "${ckpt_to}"
 
@@ -46,7 +46,7 @@ python -m molbart.predict \
   data_path=${DIR}/data/bioformer_test_tgt_wo_aug.txt \
   vocabulary_path=bart_vocab_downstream.json \
   task=backward_prediction \
-  model_path=${DIR}/chemformer_models/finetune/last_USPTO_FULL.ckpt \
+  model_path=${DIR}/ckpt/last_USPTO_FULL.ckpt \
   output_sampled_smiles=${DIR}/outputs/predicted_reactants_mode2.csv
   batch_size=64 \
   n_beams=10
